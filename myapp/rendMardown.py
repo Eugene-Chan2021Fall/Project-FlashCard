9d34e1e4319a8bce03dde4cad4aa417e920f32b8
Eugene Chan <eugene.chan@sjsu.edu>
2021-11-27 06:21:55 -0800

    adding "render Markdown" & search bar functions

---
+++ myapp/rendMardown.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import markdown
+import os
+
+@app_obj.route("/")
+def rendMark():
+    if os.path.isfile(indexmd):
+        with open(indexmd) as mdfile:
+              MDContent = markdown.markdown(mdfile.read(),
+                              extensions=['fenced_code','codehilite'])
+              else:
+                         MDContent = None
+              return render_template('index.html, MDContent=MDContent, Routes=context_routes')
+
