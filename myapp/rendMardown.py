b5fa005fb55de5014aae0562a1f3770d501a4e10
Eugene Chan <eugene.chan@sjsu.edu>
2021-11-28 21:06:06 -0800

    changes to RendMarkdown related files

--- myapp/rendMardown.py
+++ myapp/rendMardown.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 import markdown
 import os
 
+filemd = os.path.join(app_obj.root_path,'md')
+context_routes = []
+
 @app_obj.route("/")
 def rendMark():
-    if os.path.isfile(indexmd):
-        with open(indexmd) as mdfile:
+    if os.path.isfile(filemd):
+        with open(filemd) as mdfile:
               MDContent = markdown.markdown(mdfile.read(),
                               extensions=['fenced_code','codehilite'])
               else:
                          MDContent = None
-              return render_template('index.html, MDContent=MDContent, Routes=context_routes')
+              return render_template('rendMark.html, MDContent=MDContent, Routes=context_routes')
