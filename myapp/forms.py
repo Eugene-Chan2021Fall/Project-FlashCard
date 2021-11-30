8db980917df34f1e87cf81a6c74da1bde90f8c04
Seyoung <seyoung2k@naver.com>
2021-11-29 18:16:24 -0800

    Add func for mindmap

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, FileField
-from wtforms_sqlalchemy.fields import QuerySelectField
+#from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired
 
 
